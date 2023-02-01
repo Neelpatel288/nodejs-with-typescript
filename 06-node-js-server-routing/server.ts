38504dc5747235dfd0ec47aceadef3b48cfcd20e
Neelpatel288 <neelpatel288@gmail.com>
2023-02-01 14:50:42 +0530

    Intial Commit

---
+++ 06-node-js-server-routing/server.ts
@@ -0,0 +1,19 @@
+import http, { IncomingMessage, ServerResponse, Server } from 'http'
+
+const hostname: string = '127.0.0.1'
+const port: number = 5000
+
+const server: Server = http.createServer(
+	(request: IncomingMessage, response: ServerResponse) => {
+		response.statusCode = 200
+		response.setHeader('content-Type', 'text/html')
+
+		// NodeJs Routing
+
+		response.end(`<h3>hello</h3>`)
+	}
+)
+
+server.listen(port, hostname, () => {
+	console.log(`Server started at http://${hostname}:${port}`)
+})
